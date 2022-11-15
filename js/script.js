0f0ed2bac1e5a8c0d5891b8a4363cd38dc107d02
NSX2022 <111909768+NSX2022@users.noreply.github.com>
2022-11-15 11:38:46 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":980},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-mods","displayName":"Mods","link":{"linkType":"LinkTypePage","href":"#!page-mods"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
